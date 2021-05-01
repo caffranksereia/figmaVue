592f63ca0534a1f01c795707f44e54f0fd0a4177
ferafael <circuncisaofabio@icloud.com>
2021-05-01 11:19:19 -0300

    Criar as tags do html e preencher com as info

--- figma-vue/src/components/Depoimento.vue
+++ figma-vue/src/components/Depoimento.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>Depoimento</h1>
+    <div class="depoimento">
+        <h1>Depoimento</h1>
+        <div class="depoImg">
+            <img src="" alt="">
+        </div>
+        <div class="depoimentos">
+            <h4>dasdas</h4>
+        </div>
+        <div class="depoImg">
+            <img src="" alt="">
+        </div>
+        <div class="depoimentos">
+            <h4>dasdas</h4>
+        </div>
+        <div class="depoImg">
+            <img src="" alt="">
+        </div>
+        <div class="depoimentos">
+            <h4>dasdas</h4>
+        </div>
+        <div class="depoImg">
+            <img src="" alt="">
+        </div>
+        <div class="depoimentos">
+            <h4>dasdas</h4>
+        </div>
+        <input type="button" name="bck" value="">
+        <input type="button" name="next" value="">
+        <input type="radio" name="" id="">
+        <input type="radio" name="" id="">
+        <input type="radio" name="" id="">
+        <input type="radio" name="" id="">
+        
+    </div>
 </template>
+<script>
+export default {
+    name:
+        'Depoimento'
+    
+}
+</script>
