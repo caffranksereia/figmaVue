02cc45b50d39dd3e9ec2a55ac721f9b12e169ee1
ferafael <circuncisaofabio@icloud.com>
2021-05-01 11:16:01 -0300

    doc Alfataria
    fazer a parte do front em htlm sem o css.
    preencher todas as tags do html com as informações

--- figma-vue/src/components/Alfataria.vue
+++ figma-vue/src/components/Alfataria.vue
@@ -1,3 +1,29 @@
 <template>
+<div class="Alfataria">
     <h1>Alfataria</h1>
+    <p>- TOTALMENTE FEITO A MÃO SOB MEDIDA</p>
+    <div class="imgAlfataria">
+        <img src="" alt="">
+    </div>
+    <div class="list">
+        <ul>
+            <li> Fio Super 120 e 130 </li>
+            <li>Lã fria Australiana</li>
+            <li>Botões Importado</li>
+            <li>Ternos e Calças</li>
+            <li>Paletos e Smoking</li>
+            <li>Sociais e Espotivos</li>
+        </ul>
+        
+    </div>
+    <div class="btnOrcamento">
+        <button>Quero um orçamento</button>
+    </div>
+</div>
+   
 </template>
+<script>
+export default {
+    name:'Alfataria'
+}
+</script>
