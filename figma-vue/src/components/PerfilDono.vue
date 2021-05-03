2fb83022daca1a69080d76d7d000842c81f1ddf5
ferafael <circuncisaofabio@icloud.com>
2021-05-03 11:42:39 -0300

    Criar o PerfilDono

--- figma-vue/src/components/PerfilDono.vue
+++ figma-vue/src/components/PerfilDono.vue
@@ -1,4 +1,18 @@
 <template>
+<div class="perilDono">
+    <div class="img">
+        <img src="" alt="">
+    </div>
+    <div class="DescDono">
+        <div class="nDono">
+            <h1>ReneeTrajar</h1>
+        <input type="text">
+        </div>
+        <div class="discri">
+            <p><span></span></p>
+        </div>
+    </div>
+</div>
     
 </template>
 <script>
