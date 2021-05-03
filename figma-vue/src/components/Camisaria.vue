e01e51b94ea7f9fcdab1d15c65629bb021b2df3a
ferafael <circuncisaofabio@icloud.com>
2021-05-03 11:40:16 -0300

    doc Camisaria
    Divs criada e separadas

--- figma-vue/src/components/Camisaria.vue
+++ figma-vue/src/components/Camisaria.vue
@@ -1,5 +1,33 @@
 <template>
-    <h1>Camisaria</h1>
+<div class="camisarti">
+   
+    <div class="imgCamis">
+        <img src="" alt="">
+    </div>
+    <div class="infoCamisaria">
+        <h1>Camisaria</h1>
+        <p>Confessionadas artesanalmente</p>
+    </div>
+    <div class="list">
+        <ul>
+            <li> 100% Algodão  </li>
+            <li>Monograma</li>
+            <li>Slim</li>
+            <li>Nacionais e Importados</li>
+            <li>Tradicionais e sportivas</li>
+            <li>Casamento</li>
+        </ul>
+        
+    </div>
+    <div class="btnOrcamento">
+        <button>Quero um orçamento</button>
+    </div>
+
+   
+
+
+</div>
+   
 </template>
 <script>
 export default {
