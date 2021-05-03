213233eca807edd7bd9b1ff254ffe05d705bb24b
ferafael <circuncisaofabio@icloud.com>
2021-05-03 11:41:06 -0300

    doc Empresa
    Divs criada e separadas

--- figma-vue/src/components/Empresa.vue
+++ figma-vue/src/components/Empresa.vue
@@ -1,5 +1,25 @@
 <template>
+<div class="empresa">
     <h1>Empresa</h1>
+    <div class="slideEmpre">
+        <div class="h1Empresa">
+            <p>Elegante é ter um </p>
+            <h1>Alfaiate</h1>
+            <p>para chamar de seu</p>
+        </div>
+        <div class="img">
+            <img src="" alt="">
+        </div>
+        <section></section>
+    </div>
+    <div class="textApre">
+        <h2>Compre hoje, <span>pague hoje em até 3x  com 12%  de desconto</span>e tenha 10 meses para confeccionar!</h2>
+    </div>
+    <div class="btnOrcamento">
+        <button>Faça já um orçamento</button>
+    </div>
+</div>
+
 </template>
 <script>
 export default {
