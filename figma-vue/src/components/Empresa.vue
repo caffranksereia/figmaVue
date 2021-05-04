6bf78ef72e084bdf12cf5e274a70666d96647703
ferafael <circuncisaofabio@icloud.com>
2021-05-04 10:24:20 -0300

    Mudar algumas divs
    Mudar div text empresa para ficar dentro do slideEmpre para encaixar certo na pagina

--- figma-vue/src/components/Empresa.vue
+++ figma-vue/src/components/Empresa.vue
@@ -1,23 +1,23 @@
 <template>
 <div class="empresa">
-    <h1>Empresa</h1>
+    <div class="img">
+        <img src="" alt="">
+    </div>
     <div class="slideEmpre">
-        <div class="h1Empresa">
-            <p>Elegante é ter um </p>
-            <h1>Alfaiate</h1>
+        <div class="txtEmpresa">
+            <h1>Elegante é ter um </h1>
+            <p>Alfaiate</p>
             <p>para chamar de seu</p>
         </div>
-        <div class="img">
-            <img src="" alt="">
-        </div>
         <section></section>
+        
+            <h2>Compre hoje, <span>pague hoje em até 3x  com 12%  de desconto</span>e tenha 10 meses para confeccionar!</h2>
+       
+       
+            <button>Faça já um orçamento</button>
+       
     </div>
-    <div class="textApre">
-        <h2>Compre hoje, <span>pague hoje em até 3x  com 12%  de desconto</span>e tenha 10 meses para confeccionar!</h2>
-    </div>
-    <div class="btnOrcamento">
-        <button>Faça já um orçamento</button>
-    </div>
+   
 </div>
 
 </template>
@@ -26,3 +26,25 @@ export default {
     name:'Empresa'
 }
 </script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+}
+.empresa{
+    width: 100%;
+    height: 500px;
+    margin-top:10px;
+    background-color: blueviolet;
+}
+.slideEmpre{
+    background-color: turquoise;
+    margin-right: 50px;
+    margin-left: 50px;
+    margin-top: 140px;
+    width:300px;
+    height: 300px;
+}
+</style>
