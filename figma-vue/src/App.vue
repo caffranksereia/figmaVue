043633b7b978e5029c5a45ced65a89374eab2f2f
ferafael <circuncisaofabio@icloud.com>
2021-05-04 10:17:39 -0300

    Apagado o menu no app para criar separado o router-link

--- figma-vue/src/App.vue
+++ figma-vue/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="nav">
-    <router-link to="/home">Empresa</router-link> 
-    <router-link to="/camisaria">Camisaria</router-link> 
-    <router-link to="/alfataria">Alfataria</router-link> 
-    <router-link to="/depoimento">Depoimento</router-link> 
-    <router-link to="/contato">Contato</router-link> 
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
-<style>
-body{
+<style scoped>
+* {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,19 +19,5 @@ body{
   color: #2c3e50;
 }
 
-#nav {
-  background-color:blueviolet;
-  padding: 30px;
-}
 
-#nav a {
-  margin: 2px;
-  text-decoration: none;
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
